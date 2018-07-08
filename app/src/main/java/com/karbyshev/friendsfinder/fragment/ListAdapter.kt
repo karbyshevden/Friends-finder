f47aaa8e3f31cd61fba37ef90f22400efea02c93
Karbyshev Den <karbyshev.den@gmail.com>
2018-07-08 22:56:09 +0300

    recyclerview databinding

--- app/src/main/java/com/karbyshev/friendsfinder/fragment/ListAdapter.kt
+++ app/src/main/java/com/karbyshev/friendsfinder/fragment/ListAdapter.kt
@@ -2,32 +2,27 @@ package com.karbyshev.friendsfinder.fragment
 
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import com.karbyshev.friendsfinder.R
+import com.karbyshev.friendsfinder.databinding.UserBinding
 import com.karbyshev.friendsfinder.model.User
 
-class ListAdapter (private val userList: List<User>) : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+class ListAdapter(private val userList: List<User>) : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layuot, parent, false)
-        return ViewHolder(view)
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = UserBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
     }
 
-    override fun getItemCount(): Int {
-        return userList.size
-    }
+    override fun getItemCount(): Int = userList.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.name?.text = userList[position].name
-        holder.lat?.text = "Lat: " + userList[position].lat
-        holder.lng?.text = "Lng: " + userList[position].lng
-    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(userList[position])
 
-    class ViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(private val binding: UserBinding) : RecyclerView.ViewHolder(binding.root) {
 
-        val name = itemView?.findViewById<TextView>(R.id.itemNameTextView)
-        val lat = itemView?.findViewById<TextView>(R.id.itemLatTextView)
-        val lng = itemView?.findViewById<TextView>(R.id.itemLngTextView)
+        fun bind(item: User) {
+            binding.item = item
+            binding.executePendingBindings()
+        }
     }
 }
